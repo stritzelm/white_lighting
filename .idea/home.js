c0d68c12df4beb38f492cc85f0dc61183391e411
Matthew <matthew.stritzel33@gmail.com>
2019-07-15 15:56:23 -0500

    Large update: Ready for Wayne proposal; Needs paypal link

--- .idea/home.js
+++ .idea/home.js
@@ -33,21 +33,38 @@ function go_to_twitter() {
 }
 
 
-var slideIndex = 1;
-showDivs(slideIndex);
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
+
+var vidIndex = 1
+
+function plusDiv(n) {
+    showDiv(vidIndex += n)
 }
 
-function showDivs(n) {
+function showDiv(n) {
     var i;
-    var x = document.getElementsByClassName("mySlides");
+    var x = document.getElementsByClassName("myVids");
     if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length} ;
+    if (n < 1) {vidIndex = x.length} ;
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
     x[slideIndex-1].style.display = "block";
+}
+
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 3000); // Change image every 3 seconds
 }
 
